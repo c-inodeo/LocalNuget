24d0ea87ed5a039b60bca812892fbcfdfe100f32
c-inodeo <c.inodeo@arcanys.com>
2024-11-08 09:01:24 +0800

    Add project files.

---
+++ HelperLibrary/UserNotifModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserNotificationMessages.Helpers
+{
+    public class UserNotifModel
+    {
+        public string? UserId { get; set; }
+        public string? Message { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
